7d311296819ecc5e8612b14d44abec809c752f2c
nickgibson89 <nickgibson89@gmail.com>
2011-11-05 19:17:38 +0000

    Added background, got basic News screen showing.

--- SBUMobile/src/edu/sbu/sbumobile/NewsActivity.java
+++ SBUMobile/src/edu/sbu/sbumobile/NewsActivity.java
@@ -1,5 +1,15 @@
 package edu.sbu.sbumobile;
 
+import android.os.Bundle;
+
 public class NewsActivity extends BaseActivity {
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.news);		
+
+	}
 
 }
