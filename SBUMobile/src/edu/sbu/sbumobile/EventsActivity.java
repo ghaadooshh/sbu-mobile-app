f458134228b95eaa4f4facbc20e650005afe1bdc
nickgibson89 <nickgibson89@gmail.com>
2011-11-07 05:12:45 +0000

    Perpetuated the purple theme across all pages.  Also has my broken twitter code

--- SBUMobile/src/edu/sbu/sbumobile/EventsActivity.java
+++ SBUMobile/src/edu/sbu/sbumobile/EventsActivity.java
@@ -1,5 +1,14 @@
 package edu.sbu.sbumobile;
 
+import android.os.Bundle;
+
 public class EventsActivity extends BaseActivity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.events);	
+	}
 
 }
--- SBUMobile/src/edu/sbu/sbumobile/MobileApplication.java
+++ SBUMobile/src/edu/sbu/sbumobile/MobileApplication.java
@@ -1,16 +1,20 @@
 package edu.sbu.sbumobile;
 
+import winterwell.jtwitter.Twitter;
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.util.Log;
 
 public class MobileApplication extends Application implements
 		OnSharedPreferenceChangeListener {
 	private static final String TAG = MobileApplication.class.getSimpleName();
 	private SharedPreferences prefs;
-	
+	private Twitter twitter;
+	private boolean serviceRunning; // Is the Twitter service (UpdaterService) running?
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -18,21 +22,61 @@ public class MobileApplication extends Application implements
 		this.prefs.registerOnSharedPreferenceChangeListener(this);
 		Log.i(TAG, "Application Started");
 	}
+
 	/*
-	 * Prefs can be pulled by:
-	 * String username = this.prefs.getString("username", null);
-	 * 											^			^
-	 * 										from xml		default value
+	 * Prefs can be pulled by: String username =
+	 * this.prefs.getString("username", null); ^ ^ from xml default value
 	 * 
 	 * Not sure yet what prefs we have or how they will need to be handled.
-	 * 	Could use onSharedPreferenceChanged() below or use PrefsActivity.
-	 * 	See TicTacToe preferences.java for examples.
+	 * Could use onSharedPreferenceChanged() below or use PrefsActivity. See
+	 * TicTacToe preferences.java for examples.
 	 */
 	@Override
-	public synchronized void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+	public synchronized void onSharedPreferenceChanged(
+			SharedPreferences sharedPreferences, String key) {
 		Log.i(TAG, "onSharedPreferenceChanged");
+		
+		// Clear the twitter account so that it will reinitialize with the new preferences.
+		this.twitter = null;
 	}
-	
+
+	/*
+	 * Method to send a user name and password to Twitter and get an account
+	 * handle in return. Synchronized so that it will be the only object
+	 * interacting with Twitter across all activities. See original source code
+	 * in YambaApplication.java
+	 */
+	public synchronized Twitter getTwitter() { //
+		if (this.twitter == null) {
+
+			// Values have been hardcoded for now
+			String username = "student"; // original value: this.prefs.getString("username", "");
+			String password = "password"; // original value: this.prefs.getString("password", "");
+			String apiRoot = "http://yamba.marakana.com/api"; // original value: prefs.getString("apiRoot", "http://yamba.marakana.com/api");
+			
+			if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)
+					&& !TextUtils.isEmpty(apiRoot)) {
+				this.twitter = new Twitter(username, password);
+				this.twitter.setAPIRootUrl(apiRoot);
+			}
+		}
+		return this.twitter;
+	}
+
+	/*
+	 * Check the status of the Twitter running flag.
+	 */
+	public boolean isServiceRunning() { //
+		return serviceRunning;
+	}
+
+	/*
+	 * Set the status of the Twitter running flag.
+	 */
+	public void setServiceRunning(boolean serviceRunning) { //
+		this.serviceRunning = serviceRunning;
+	}
+
 	@Override
 	public void onTerminate() {
 		super.onTerminate();
