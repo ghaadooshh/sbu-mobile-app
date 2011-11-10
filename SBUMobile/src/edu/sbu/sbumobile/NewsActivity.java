deee684665c38794245e003d115f5dbeda43fc37
nickgibson89@gmail.com <nickgibson89@gmail.com@8546e8bf-0e36-b6a2-b29f-31962b93fddb>
2011-11-10 14:51:23 +0000

    Obliterated that useless piece-o-crap winterwell stuff.  I'll try again with the Twitter4J library ASAP.

--- SBUMobile/src/edu/sbu/sbumobile/NewsActivity.java
+++ SBUMobile/src/edu/sbu/sbumobile/NewsActivity.java
@@ -1,6 +1,5 @@
 package edu.sbu.sbumobile;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class NewsActivity extends BaseActivity {
@@ -9,10 +8,7 @@ public class NewsActivity extends BaseActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.news);
-		
-		// Substitute for the start/stop menu system in Yamba; updater service started directly.
-		startService(new Intent(this, UpdaterService.class));		
+		setContentView(R.layout.news);		
 	}
 
 }
