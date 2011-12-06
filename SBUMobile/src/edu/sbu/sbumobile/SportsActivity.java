5ab036c5623a588f3b74bf665c0f1f17910303cf
nickgibson89@gmail.com <nickgibson89@gmail.com@8546e8bf-0e36-b6a2-b29f-31962b93fddb>
2011-12-06 19:28:08 +0000

    Threaded sportsactivity

--- SBUMobile/src/edu/sbu/sbumobile/SportsActivity.java
+++ SBUMobile/src/edu/sbu/sbumobile/SportsActivity.java
@@ -3,7 +3,6 @@ package edu.sbu.sbumobile;
 import java.net.URL;
 import java.util.ArrayList;
 
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
@@ -18,33 +17,39 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class SportsActivity extends Activity {
-
+	ListView listView;
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.sports);
-        
-        ArrayList<Tweet> tweets = getTweets("from:sbubearcats", 1);
-        ListView listView = (ListView) findViewById(R.id.ListViewId);
-        listView.setAdapter(new UserItemAdapter(this, R.layout.listitem, tweets));
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.news);
+
+//		ArrayList<Tweet> tweets = getTweets("from:sbuniv", 1);
+		listView = (ListView) findViewById(R.id.ListViewId);
+//		listView.setAdapter(new UserItemAdapter(this, R.layout.listitem, tweets));
+
+		getBackgroundTweets task = new getBackgroundTweets();
+		task.execute("from:sbubearcats");
+	}
 
 	public class UserItemAdapter extends ArrayAdapter<Tweet> {
 		private ArrayList<Tweet> tweets;
 
-		public UserItemAdapter(Context context, int textViewResourceId, ArrayList<Tweet> tweets) {
+		public UserItemAdapter(Context context, int textViewResourceId,
+				ArrayList<Tweet> tweets) {
 			super(context, textViewResourceId, tweets);
 			this.tweets = tweets;
 		}
@@ -53,7 +58,7 @@ public class SportsActivity extends Activity {
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View v = convertView;
 			if (v == null) {
-				LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 				v = vi.inflate(R.layout.listitem, null);
 			}
 
@@ -67,14 +72,16 @@ public class SportsActivity extends Activity {
 					username.setText(tweet.username);
 				}
 
-				if(message != null) {
+				if (message != null) {
 					message.setText(tweet.message);
 				}
-				
-				if(image != null) {
-					//image.setImageBitmap(getBitmap(tweet.image_url));
-					image.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.bearcat_tweet_icon));
+
+				if (image != null) {
+					image.setImageBitmap(BitmapFactory.decodeResource(
+							getResources(), R.drawable.seal_tweet_icon));
 				}
+
+				message.setMovementMethod(LinkMovementMethod.getInstance());
 				message.setOnClickListener(new OnClickListener() {
 					@Override
 					public void onClick(View v) {
@@ -98,66 +105,97 @@ public class SportsActivity extends Activity {
 	public Bitmap getBitmap(String bitmapUrl) {
 		try {
 			URL url = new URL(bitmapUrl);
-			return BitmapFactory.decodeStream(url.openConnection() .getInputStream()); 
+			return BitmapFactory.decodeStream(url.openConnection()
+					.getInputStream());
+		} catch (Exception ex) {
+			return null;
 		}
-		catch(Exception ex) {return null;}
+	}
+
+
+	private class getBackgroundTweets extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			
+			String response = "";
+			DefaultHttpClient client = new DefaultHttpClient();
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			for (String searchTerm : urls) {
+				
+				String searchUrl = "http://search.twitter.com/search.json?q=@"
+						+ searchTerm + "&rpp=100&page=" + "1";
+				HttpGet httpGet = new HttpGet(searchUrl);
+				try{
+					response = client.execute(httpGet, responseHandler);
+				}catch(Exception ex) {
+					ex.printStackTrace();
+					System.out.println("Exception: " + ex);
+				}
+		        
+			}
+			return response;
+		}
+	    
+		@Override
+		protected void onPostExecute(String result) {
+			getTweets(result);
+		}
 	}
 	
-	public ArrayList<Tweet> getTweets(String searchTerm, int page) {
-		String searchUrl = "http://search.twitter.com/search.json?q=@" 
-							+ searchTerm + "&rpp=100&page=" + page;
-		
+	public void getTweets(String responseBody) {
+//		String searchUrl = "http://search.twitter.com/search.json?q=@"
+//				+ searchTerm + "&rpp=100&page=" + page;
+//
 		ArrayList<Tweet> tweets = new ArrayList<Tweet>();
-		
-		HttpClient client = new  DefaultHttpClient();
-		HttpGet get = new HttpGet(searchUrl);
-	      
-		ResponseHandler<String> responseHandler = new BasicResponseHandler();
-
-		String responseBody = null;
-		try{
-			responseBody = client.execute(get, responseHandler);
-		}catch(Exception ex) {
-			ex.printStackTrace();
-		}
+//
+//		HttpClient client = new DefaultHttpClient();
+//		HttpGet get = new HttpGet(searchUrl);
+//
+//		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+//
+//		String responseBody = null;
+//		try {
+//			responseBody = client.execute(get, responseHandler);
+//			System.out.println(responseBody);
+//		} catch (Exception ex) {
+//			ex.printStackTrace();
+//		}
 
 		JSONObject jsonObject = null;
-		JSONParser parser=new JSONParser();
-		
+		JSONParser parser = new JSONParser();
+
 		try {
 			Object obj = parser.parse(responseBody);
-			jsonObject=(JSONObject)obj;
-			
-		}catch(Exception ex){
-			Log.v("TEST","Exception: " + ex.getMessage());
-		}
-		
-		JSONArray arr = null;
-		
-		try {
-			Object j = jsonObject.get("results");
-			arr = (JSONArray)j;
-		}catch(Exception ex){
-			Log.v("TEST","Exception: " + ex.getMessage());
+			jsonObject = (JSONObject) obj;
+
+		} catch (Exception ex) {
+			Log.v("TEST", "Exception: " + ex.getMessage());
 		}
 
-		for(Object t : arr) {
-			Tweet tweet = new Tweet(
-					((JSONObject)t).get("from_user").toString(),
-					((JSONObject)t).get("text").toString(),
-					((JSONObject)t).get("profile_image_url").toString()
-					);
+		JSONArray arr = null;
+
+		try {
+			Object j = jsonObject.get("results");
+			arr = (JSONArray) j;
+		} catch (Exception ex) {
+			Log.v("TEST", "Exception: " + ex.getMessage());
+		}
+
+		for (Object t : arr) {
+			Tweet tweet = new Tweet(((JSONObject) t).get("from_user")
+					.toString(), ((JSONObject) t).get("text").toString(),
+					((JSONObject) t).get("profile_image_url").toString());
 			tweets.add(tweet);
 		}
-		
-		return tweets;
+
+		listView.setAdapter(new UserItemAdapter(this, R.layout.listitem, tweets));
 	}
-	
+
 	public class Tweet {
 		public String username;
 		public String message;
 		public String image_url;
-		
+
 		public Tweet(String username, String message, String url) {
 			this.username = username;
 			this.message = message;
